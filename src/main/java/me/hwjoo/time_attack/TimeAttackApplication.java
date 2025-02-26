c04008db6aa3c1bc521f0745cd0a0359706ffb7e
hwjoo <jhw664089@gmail.com>
2025-02-26 13:06:14 +0900

    init project

---
+++ src/main/java/me/hwjoo/time_attack/TimeAttackApplication.java
@@ -0,0 +1,13 @@
+package me.hwjoo.time_attack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeAttackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeAttackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/hwjoo/time_attack/TimeAttackApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hwjoo.time_attack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeAttackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
